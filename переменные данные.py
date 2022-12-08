ce5dfc41ee223eb5beead508a43e3f95efab9470
alexandr <wmsuvorov@gmail.com>
2022-12-09 00:39:03 +0200

    домашнее задание

---
+++ переменные данные.py
@@ -0,0 +1,12 @@
+Name = "Alexandr"
+print(type(Name))
+Name = False
+print(type(Name))
+Name = 22
+print(type(Name))
+Name = 34.3
+print(type(Name))
+Name = 2+3j
+print(type(Name))
+Name = b"bytes"
+print(type(Name))
